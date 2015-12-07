fa31c5e8fca82c20b3f6d9dcb6cd4b235433258f
Nicholas Russell <nick.f.russell@gmail.com>
2015-12-07 14:12:22 -0600

    Add useful lein plugins

--- link/.lein/profiles.clj
+++ link/.lein/profiles.clj
@@ -1,2 +1,6 @@
-{:user {:plugins [[cider/cider-nrepl "0.9.0-SNAPSHOT"]]
-        :dependencies [[org.clojure/tools.nrepl "0.2.7"]]}}
+{:user {:plugins [[cider/cider-nrepl "0.10.0"]
+                  [jonase/eastwood "0.2.1"]
+                  [lein-kibit "0.1.2"]
+                  [lein-ancient "0.6.8"]
+                  [lein-bikeshed "0.2.0"]]
+        :dependencies [[org.clojure/tools.nrepl "0.2.12"]]}}
