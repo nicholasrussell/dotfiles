86d6a1590a49f0e449b2e4019a7bcff8e5dc2db3
Nick Russell <nick.f.russell@gmail.com>
2023-02-03 21:36:28 -0600

    nvim

--- source/1_path.sh
+++ source/1_path.sh
@@ -2,6 +2,9 @@ export JENV_PATH="$HOME/.jenv"
 export NVM_DIR="$HOME/.nvm"
 export PYENV_ROOT="$HOME/.pyenv"
 
+export XDG_CONFIG_HOME="$HOME/.config"
+export XDG_DATA_HOME="$HOME/.local/share"
+
 if [ -d "$HOME/bin" ]; then PATH="$HOME/bin:$PATH"; fi
 if [ -d $PYENV_ROOT ]; then PATH="$PYENV_ROOT/bin:$PATH"; fi
 if [ -d $JENV_PATH ]; then PATH="$HOME/.jenv/bin:$PATH"; fi
--- tools/4_editors.sh
+++ tools/4_editors.sh
@@ -28,6 +28,16 @@ install_emacs
 
 # vim
 function install_vim {
+    log_info "Installing nvm..."
+    if is_macos; then
+        idempotent_brew_installbrew install neovim
+    else
+        add_ppa ppa:neovim-ppa/unstable
+        apt_update 
+        apt_install neovim
+    fi
+    log_info "Finished installing nvim."
+    
     log_info "Configuring vim..."
     if [ ! -d ~/.vim/bundle/Vundle.vim ]; then
         git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
@@ -35,6 +45,12 @@ function install_vim {
         log_info "Installed plugins."
     fi
     log_info "Finished configuring vim."
+
+    log_info "Configuring nvim..."
+    if [ ! -d ~/.local/share/nvim/site ]; then
+        git clone --depth 1 https://github.com/wbthomason/packer.nvim ~/.local/share/nvim/site/pack/packer/start/packer.nvim
+    fi
+    log_info "Finished configuring nvim."
 }
 
 install_vim
