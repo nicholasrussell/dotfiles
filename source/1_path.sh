7a69b2ec2557fcc6edf3a5120d52ff374b6bd5d3
Nicholas Russell <nick.f.russell@gmail.com>
2025-07-31 21:15:02 -0500

    .local/bin to path

--- source/1_path.sh
+++ source/1_path.sh
@@ -23,6 +23,7 @@ export PYENV_ROOT="$HOME/.pyenv"
 if [ -d "$HOME/bin" ]; then PATH="$HOME/bin:$PATH"; fi
 if [ -d "$PYENV_ROOT" ]; then PATH="$PYENV_ROOT/bin:$PATH"; fi
 if [ -d "$JENV_PATH" ]; then PATH="$HOME/.jenv/bin:$PATH"; fi
+if [ -d "$HOME/.local/bin" ]; then PATH="$PATH:$HOME/.local/bin"; fi
 
 case ":$PATH:" in
   *":$PNPM_HOME:"*) ;;
