edea4a2298dee15528d805811ad936c8d968ebe6
nicholasrussell <nick.f.russell@gmail.com>
2025-08-07 16:18:32 -0500

    gpg_tty

--- source/2_eval.sh
+++ source/2_eval.sh
@@ -14,3 +14,5 @@ if [[ "$INSIDE_EMACS" = 'vterm' ]] &&
   [[ -f ${EMACS_VTERM_PATH}/etc/emacs-vterm-bash.sh ]]; then
   source "${EMACS_VTERM_PATH}/etc/emacs-vterm-bash.sh"
 fi
+
+export GPG_TTY=$(tty)
